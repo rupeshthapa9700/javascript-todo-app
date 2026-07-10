17eefa70d6d615e06a6788246d08cec8ad01337b
Rupesh Thapa <rupeshthapa@Rupeshs-MacBook-Air.local>
2026-07-10 22:36:44 +0545

    Add task and show dynamically

---
+++ script.js
@@ -0,0 +1,25 @@
+const taskForm = document.querySelector("#task-form");
+const taskInput = document.querySelector("#add-task-input");
+const taskContainer = document.querySelector("#task-container");
+
+taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const taskText = taskInput.value;
+
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        <input type="checkbox">
+        <span>${taskText}</span>
+        <button class="edit-btn">Edit</button>
+        <button class="delete-btn">Delete</button>
+    `;
+    taskContainer.append(li);
+    taskInput.value = "";
+});
+
+taskContainer.addEventListener("click", (event) => {
+    if(event.target.classList.contains("delete-btn")){
+        event.target.parentElement.remove();
+    }
+});
